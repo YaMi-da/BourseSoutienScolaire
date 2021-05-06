a74f80b420bfe736f0406fe0dc15ca0cf26d32db
YaMi-da <aya-anime@outlook.fr>
2021-05-06 12:56:42 +0000

    . . .

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,34 +1,59 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Soutien Scolaire</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('template/css/custom-css.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="">
+
+    
+    <div class="container2" id="container">
+        <div>
+            <x-jet-validation-errors class="mb-4" />
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+                <h1>Mot de Passe oublié ?</h1>
+                <h4>Pas de problème. Saisissez votre adresse email ci-dessous, nous vous enverrons un email vous permettant de changer votre mot de passe</h6>
+                <x-jet-input id="email" name="email" type="email" placeholder="Email" :value="old('email')" required autofocus/>
+                <x-jet-button>Réinitialiser mot de passe</x-jet-button>
+            </form>
         </div>
+    </div>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+<footer>
+	<p>
+		
+	</p>
+</footer>
 
-        <x-jet-validation-errors class="mb-4" />
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
-            <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('template/js/custom-js.js') }}"></script>
 
-            <div class="flex items-center justify-end mt-4">
-                <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+</body>
+
+</html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,87 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <x-jet-validation-errors class="mb-4" />
+<head>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <title>Soutien Scolaire</title>
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('template/css/custom-css.css') }}" rel="stylesheet">
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+</head>
 
-            <div class="flex items-center justify-end mt-4">
+<body class="">
+    
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <h1>Créez Votre Compte</h1>
+                <x-jet-validation-errors class="mb-4" />
+                <x-jet-input id="name" name="name" type="text" placeholder="Nom Complet" :value="old('name')" required autofocus autocomplete="name"/>
+                <x-jet-input id="email" name="email" type="email" placeholder="Email" :value="old('email')" required/>
+                <x-jet-input id="password" name="password" type="password" placeholder="Mot de Passe" required autocomplete="new-password"/>
+                <x-jet-input id="password_confirmation" name="password_confirmation" type="password" placeholder="Confirmer Mode de Passe" required autocomplete="new-password"/>
+                <x-jet-button>S'inscrire</x-jet-button>
+            </form>
+        </div>
+        <div class="form-container sign-in-container">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <h1>Connectez-vous !</h1>
+                <x-jet-validation-errors class="mb-4" />
+                <x-jet-input id="email" name="email" type="email" placeholder="Email" :value="old('email')" required autofocus />
+                <x-jet-input id="password" name="password" type="password" placeholder="Mot de Passe" required autocomplete="current-password"/>
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a href="{{ route('password.request') }}">Mot de Passe oublié ?</a>
                 @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
+                <x-jet-button>Se Connecter</x-jet-button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                    <h1>Bienvenue !</h1>
+                    <p>Vous avez un compte ?</p>
+                    <button class="ghost" id="signIn">Connectez-vous !</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Nouveau ici ?</h1>
+                    <p>Créez votre compte !</p>
+                    <button class="ghost" id="signUp">S'inscrire</button>
+                </div>
             </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+    </div>
+
+<footer>
+	<p>
+		
+	</p>
+</footer>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ asset('template/js/custom-js.js') }}"></script>
+
+</body>
+
+</html>
